1295b7c16049c8f4be67b72585e5680636a9cc54
Liviu <moiseil@b2b.regn.net>
2023-10-04 15:48:07 +0300

    Adding catalog service.

---
+++ catalog-service/src/main.ts
@@ -0,0 +1,34 @@
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+require('dotenv').config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: ['category', 'product'],
+      protoPath: [
+        'node_modules/protos/proto/category.proto',
+        'node_modules/protos/proto/product.proto',
+      ],
+      url: '0.0.0.0:5002',
+    },
+  });
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      consumer: {
+        groupId: 'catalog-service-group',
+      },
+      client: {
+        brokers: [process.env.KAFKA_BROKER_URL || 'localhost:9092'],
+      },
+    },
+  });
+  await app.startAllMicroservices();
+}
+
+bootstrap();
