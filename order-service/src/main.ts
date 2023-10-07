27a7ec3f46254946b5ee006f78e91e0326222d32
Liviu <moiseil@b2b.regn.net>
2023-10-07 10:54:34 +0300

    Adding order service.

---
+++ order-service/src/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { OrderModule } from './order.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { protobufPackage } from './proto/order.pb';
+
+require('dotenv').config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(OrderModule);
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: protobufPackage,
+        protoPath: 'node_modules/protos/proto/order.proto',
+        url: '0.0.0.0:5003',
+      },
+    },
+    { inheritAppConfig: true },
+  );
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [process.env.KAFKA_BROKER_URL || 'localhost:9092'],
+      },
+      consumer: {
+        groupId: 'order-service-group',
+      },
+    },
+  });
+  await app.startAllMicroservices();
+}
+
+bootstrap();
